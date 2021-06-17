cdd753d7acea6d354afd0b98d67489fb837ce527
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-17 05:57:50 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/** 
- *main - this is the main function 
+/**
+ *main - this is the main function
  *uses the puts function to pass string to stdout
+ *
+ *Return: 0
 */
 int main(void)
 {
 	char s[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(s);
-	return(0);
+	return (0);
 }
