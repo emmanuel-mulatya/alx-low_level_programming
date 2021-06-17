58d2c8021ea1d935a39993f4f9ee205ba8c27bf0
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-17 08:34:54 -0700

    printing to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - This is th beginning of the programm
+ *it takes no arguments
+ *
+ *prints a string to stderr
+ *
+ *Return: 1 error/fail
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-
+10-19");
+	return (1);
+}
