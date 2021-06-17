2903c77f294b8ac8e936411b37c6a7725907235b
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-17 05:46:10 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+/* main - this is the main function */
 int main(void)
 {
-	char s[] ="Programming is like building a multilingual puzzle";
+	char s[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(s);
-	return(0);
+	return 0;
 }
