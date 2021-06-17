cee8365e237e177994c1f6641960115cfe423f01
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-17 09:22:02 -0700

    system calls to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+cr4#include <stdio.h>
 #include <unistd.h> /*calls system call that write data out of buffer*/
 
 /**
@@ -11,6 +11,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
