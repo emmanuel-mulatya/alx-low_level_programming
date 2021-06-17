c6dd4118a6b17ce747f541a1b7a3ea0374f2ccfc
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-17 06:07:37 -0700

    using printf to print a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - this is the main function
+ *takes no arguments
+ *
+ *printing values of a string using printf
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is apiece of art,\n");
+	return (0);
+}
