2c30e019652f75c51bf0c93b3fe5cc48009b9701
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-18 07:32:03 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main class
+ * it takes no arguments
+ *
+ *printing numbers from their ASCII values
+ *
+ *Return: 0
+ **/
+
+int main(void)
+{
+	int zero, nine, nl;
+
+	zero = 48;
+	nine = 57;
+	nl = 10;
+
+	for (int 1 = zero; i <= nine; i++)
+		putchar(i);
+
+	putchar(nl);
+	return (0);
+}
