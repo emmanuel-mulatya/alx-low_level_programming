b2b1c51e1a808d0137cced51406223f849939095
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-18 07:49:07 -0700

    prnting alphabets inversely

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *takes no arguments
+ *
+ *prints alphabets in reverse order
+ *
+ *Return: 0
+ **/
+
+int main(void)
+{
+	int i, a, z, nl;
+
+	a = 97;
+	z - 122;
+	nl = 10;
+
+	for (i = z; i >= a; i--)
+		putchar(i);
+
+	putchar(nl);
+	return (0);
+}
