0571d23e6123aaf1675104dfbc5dcfd664ae89c8
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-18 05:55:15 -0700

    printing upper and lower case

---
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main - this is the main function
+ *takes no arguments
+ *
+ *prints all lowercase alphabets
+ *
+ *Return: 0
+ **/
+
+int main(void)
+{
+	int alp, next;
+
+	alp = 97;
+	next = 10;
+
+	while (alp <= 122)
+	{
+		putchar(alp);
+		alp++;
+	}
+
+	alp = 65;
+
+	while (alp <= 90)
+	{
+		putchar(alp);
+		alp++;
+	}
+	putchar(next);
+	return (0);
+}
