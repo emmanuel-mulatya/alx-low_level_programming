4583e28da3a552536c329c9bb27b4b434f55e037
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-18 04:38:55 -0700

    check if its positive or negative

---
+++ 0x01-variables_or_negative.c/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - this is the main function
+ *it takes no arguments
+ *
+ *uses the if..else statement to test if a number is positive negative or zero
+ *
+ *Return: 0
+ **/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
