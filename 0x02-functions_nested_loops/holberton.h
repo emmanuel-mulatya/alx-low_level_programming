93022b03aff43aec381008d2b0af7c89c6d27228
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-22 11:57:26 +0000

    prints lowercase letters 10 times

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,11 +1,17 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include <stdio.h>
 #include "_putchar.c"
 
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
 /**
- *print_alphabet() - prints lowercase alphabet letters
- *
- *Return: Nothing
- */
+  *print_alphabet -  prints lowercase alphabet
+  *Takes no arguments
+  *
+  *reads ascii values of a-z and uses the _putchar function to print to stdout)
+  */
 
 void print_alphabet(void)
 {
@@ -14,8 +20,33 @@ void print_alphabet(void)
 	while (a <= 122)
 	{
 		_putchar(a);
-		a++
+		a++;
 
 	}
 	_putchar(10);
 }
+
+/**
+  *print_alphabet_x10 - prints lowercase 10 times
+  */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int i = 0;
+
+	while (i < 9)
+	{
+		a = 97;
+
+		while (a <= 122)
+		{
+			_putchar(a);
+			a++;
+		}
+		putchar(10);
+		i++;
+	}
+}
+
+#endif
