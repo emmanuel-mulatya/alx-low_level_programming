3090912abda6587568acf1181dbc1ea5b197a585
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-23 15:08:40 +0000

    multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((mult / 10) % 10) + '0');
+					_putchar((mult / 10) % 10 + '0');
 					_putchar((mult % 10) + '0');
 				}
 				else if (mult > 99 && mult < 1000)
