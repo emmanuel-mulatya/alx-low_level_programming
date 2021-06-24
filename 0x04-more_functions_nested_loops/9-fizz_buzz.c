0de858f3114c3480bf567f314b18d385ab1df876
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-24 14:04:01 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  *main - this the main function
+  *
+  *Return: Always zero;
+  */
+
+int main(void)
+{
+	int counter;
+
+	counter = 1;
+
+	while (counter <= 100)
+	{
+		if (((counter % 3) == 0) && ((counter % 5) == 0))
+			printf("FizzBuzz ");
+		else if ((counter % 3) == 0)
+			printf("Fizz ");
+		else if ((counter % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", counter);
+		counter++;
+	}
+	printf("\n");
+	return (0);
+}
