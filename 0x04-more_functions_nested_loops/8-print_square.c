ca3f6bf34a609c82c974d15b1cace067bb7c3beb
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-24 13:38:03 +0000

    printing a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+  *print_square - prints a square
+  *@size: takes one argument of int  type
+  */
+
+void print_square(int size)
+{
+	int counter;
+	int nl;
+
+	counter = 0;
+
+	if (size > 0)
+	{
+		while (counter < size)
+		{
+			for (nl = 0; nl < size; nl++)
+				_putchar('#');
+			_putcharchar('\n');
+			counter++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
