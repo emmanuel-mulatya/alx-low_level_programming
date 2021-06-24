f501d2506b60a9bd9c880fc22dd4581f1bbc69db
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-24 11:46:02 +0000

    print to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+  *more_numbers - prints 0 - 14
+  */
+
+void more_numbers(void)
+{
+	int i, n, second, max;
+
+	n = '0';
+	second = 0;
+	max = '9';
+
+	for (i = 0; i < 10 < i++)
+	{
+		while (second < 2)
+		{
+			while (n <= limit)
+			{
+				if (n == 4)
+					_putchar('1');
+				_putchar(n);
+				n++;
+			}
+			second++;
+			max = '4';
+			n = '0'
+		}
+		_putchar(10);
+		second = 0;
+		n = '0';
+		max = '9';
+	}
+}
