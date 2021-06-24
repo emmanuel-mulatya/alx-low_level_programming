e923593d1885327d6d0a0a37aa626fbfe786594d
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-24 17:31:56 +0000

    largest

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,6 +2,6 @@
 #define HOLBERTON_H
 #include <stdio.h>
 void positive_or_negative(int i);
-int largest_number(int a, int b. int c);
+int largest_number(int a, int b, int c);
 
 #endif /* HOLBERTON_h*/
