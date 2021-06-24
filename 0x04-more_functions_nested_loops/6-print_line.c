03d445ac3dcb872ee2562b425338103351b3a805
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-24 11:58:25 +0000

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+  *print_line - prints a line
+  *@n: takes an argument of int type
+  */
+
+void print_line(int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar(10);
+}
