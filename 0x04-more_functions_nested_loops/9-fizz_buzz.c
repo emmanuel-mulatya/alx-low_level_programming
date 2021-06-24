18bac7afb7cd3757dcc31590f578443cf5b11b9d
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-24 15:40:45 +0000

    fizzy buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,21 @@ int main(void)
 	while (counter <= 100)
 	{
 		if (((counter % 3) == 0) && ((counter % 5) == 0))
+		{
 			printf("FizzBuzz ");
+		}
 		else if ((counter % 3) == 0)
+		{
 			printf("Fizz ");
+		}
 		else if ((counter % 5) == 0)
+		{
 			printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", counter);
+		}
 		counter++;
 	}
 	printf("\n");
