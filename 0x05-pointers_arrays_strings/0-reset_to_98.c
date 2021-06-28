e13559e3e56b3bd95a51881905d580524e1a1226
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-28 12:35:35 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *reset_to_98 - takes a pointer and rests its value to 98
+  *@n: pointer of int data type
+  *Returns: nothing
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
