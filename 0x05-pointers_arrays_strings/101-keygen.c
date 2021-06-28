02ab6024e98a188aba10a8a98e3cf712edd9f471
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-28 20:20:25 +0000

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,65 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  *randomPass - prints random password
+  *@N:length of password
+  *Return: nothing
+  */
+
+
+void randomPass( int N)
+{
+	int i ;
+	int random;
+
+	i = random = 0;
+
+	srand((unsigned int)(time(NULL)));
+	char numbers[] = "0123456789";
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char symbol[] = "!@#$%^&*?";
+	char password[N];
+
+	random = rand() % 4;
+
+	for (i = 0; i < N; i++)
+	{
+		if (random == 1)
+		{
+			password[i] = numbers[rand]() % 10];
+			random = rand() % 4;
+			printf("%c", password[1]);
+		}
+		else if (random == 2)
+		{
+			password[i] = symbols[rand]() % 8];
+			random = rand() % 4;
+			printf("%c", password[1]);
+		}
+		else if (random == 3)
+		{
+			password[i] = upper[rand]() % 26];
+			random = rand() % 4;
+			printf("%c", password[1]);
+		}
+		else (random == 1)
+		{
+			password[i] = numbers[rand]() % 26];
+			random = rand() % 4;
+			printf("%c", password[1]);
+		}
+	}
+}
+
+int main(void)
+{
+	int N = 10;
+
+	randomPass(N);
+	return (0);
+}
+
