f0fe9660443554ca87de9d0d54a636c02c001664
emmanuel-mulatya <emulatya7@gmail.com>
2021-06-30 16:52:02 +0000

    function to convert lowercase numbers to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  *string_toupper - changes all lowercase letters to uppercase
+  *@str: string to be checked
+  *
+  *Return: str
+  */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+
+	return (str);
+}
