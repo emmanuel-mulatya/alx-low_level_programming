a870f2f931b1b2bde6a753b3e2ca072d9280d31b
emmanuel-mulatya <emulatya7@gmail.com>
2021-07-01 10:54:32 +0000

    strcpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,8 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	i = j = 0;
-
-	while (src[i])
-		j++;
 	for (i = 0; (i < n) && (src[i] != '\0'); i++)
 		dest[i] = src[i];
 	for (; i < n; i++)
