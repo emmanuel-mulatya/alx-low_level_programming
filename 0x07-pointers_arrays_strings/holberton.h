2893af458f6cf380c3e2e449506dedc0a1d55d48
emmanuel-mulatya <emulatya7@gmail.com>
2021-07-05 14:47:38 +0000

    holberton.h

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_mecpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystick, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+#endif /* HOLBERTON_H */
