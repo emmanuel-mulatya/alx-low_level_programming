ab2350aaa61f8be283b4d35df39548e105076d32
emmanuel-mulatya <emulatya7@gmail.com>
2021-07-19 10:09:07 +0000

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  *init_dog - prints info about a dog 
+  *@d: dog structure
+  *@name: Dog's name
+  *@age: Dog's age
+  *@owner: Dog's owner
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
