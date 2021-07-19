b44fffbc013710b1837184de3ed8e692d6ed6356
emmanuel-mulatya <emulatya7@gmail.com>
2021-07-19 09:45:18 +0000

    dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - shows a dog info
+  *@name: Dog's name
+  *@age: Dog's age
+  *@owner: Dog's owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H */
